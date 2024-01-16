d03928398a9c819af9a955f5d419fb4a6f233c94
Arley Souto <macx15@hotmail.com>
2024-01-16 00:45:24 -0300

    feat: finalizado o layout

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 }
 
 class Home extends StatefulWidget {
-  const Home({super.key});
+  Home({super.key});
 
   @override
   State<Home> createState() => _HomeState();
@@ -17,52 +17,86 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Calculadora de IMC',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        appBar: AppBar(
+          title: const Text(
+            'Calculadora de IMC',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.refresh,
+                color: Colors.white,
+              ),
+            )
+          ],
         ),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.refresh,
-              color: Colors.white,
-            ),
-          )
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment
-              .stretch, //vai ocupar toda a largura vertical da tela
-          children: [
-            Icon(
-              Icons.person_outline,
-              size: 120,
-              color: Colors.green,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Peso em (Kg)',
-                labelStyle: TextStyle(color: Colors.green),
-              ),
-              style: TextStyle(color: Colors.green, fontSize: 25),
-              textAlign: TextAlign.center,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Altura (cm)',
-                labelStyle: TextStyle(color: Colors.green),
-              ),
-              style: TextStyle(color: Colors.green, fontSize: 25),
-              textAlign: TextAlign.center,
-            ),
-          ]),
-    );
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment
+                  .stretch, //vai ocupar toda a largura vertical da tela
+              children: [
+                const Icon(
+                  Icons.person_outline,
+                  size: 100,
+                  color: Colors.green,
+                ),
+                const SizedBox(height: 20),
+                const TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: 'Peso em (Kg)',
+                      labelStyle: TextStyle(color: Colors.green),
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green)),
+                      focusColor: Colors.green,
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green))),
+                  style: TextStyle(color: Colors.green),
+                ),
+                const SizedBox(height: 20),
+                const TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: 'Altura (cm)',
+                      labelStyle: TextStyle(color: Colors.green),
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green)),
+                      focusColor: Colors.green,
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green))),
+                  style: TextStyle(color: Colors.green),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Calcular',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            const MaterialStatePropertyAll<Color>(Colors.green),
+                        shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(3)))),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Info',
+                  style: TextStyle(color: Colors.green, fontSize: 17),
+                )
+              ]),
+        ));
   }
 }
